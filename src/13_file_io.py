079cb1f54a7bdeed4169976122ca99574dfbf8cc
Reed James <reedtjames1234@gmail.com>
2019-10-29 23:07:05 -0500

    13_file_io.py complete working on calendar

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,25 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+print(f.read())
 
+f.close()
+print("##############################")
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'r+') as b:
+    b.write('heres the first line')
+    b.write('heres the second line')
+    b.write('heres the third line')
+    print(b.read())
+
+b.closed
+# f = open('bar.txt', 'r+')
+# f.write('heres the first line')
+# print(f.read())
+# f.close()
